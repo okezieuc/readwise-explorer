d391e14a4b3d8672f2542c685f8dd95289712b5f
okezieuc <okezieuc@users.noreply.github.com>
2023-12-03 15:17:42 -0600

    create API that returns similar highlights

--- worker/src/index.js
+++ worker/src/index.js
@@ -65,6 +65,54 @@ app.post('/fetch', async (c) => {
 	return c.json({ success: 'uploaded all highlights to vectorize' });
 });
 
+app.get('/similar', async (c) => {
+	const ai = new Ai(c.env.AI);
+
+	// get the ID of the document the request was sent for
+	const highlightId = c.req.query('highlightId');
+	if (!highlightId) {
+		return c.json(
+			{
+				error: 'send the id of a document',
+			},
+			400
+		);
+	}
+
+	// get the contents of the highlight with that ID
+	const requestedHighlightContentQuery = `SELECT * FROM highlights WHERE id = (?);`;
+	const { results } = await c.env.DB.prepare(requestedHighlightContentQuery).bind(highlightId).all();
+	const relevantHighlight = results.length ? results[0].text : null;
+
+	if (!relevantHighlight) {
+		return c.json({
+			error: 'no document exists with the ID provided',
+		});
+	}
+
+	// create a vector embedding for the relevant content
+	const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [relevantHighlight] });
+	const relevantHighlightEmbedding = data[0];
+	if (!relevantHighlightEmbedding) {
+		return c.text('Failed to generate vector embedding', 500);
+	}
+
+	// query vectorize for similar content
+	const vectorQuery = await c.env.VECTORIZE_INDEX.query(relevantHighlightEmbedding, { topK: 4 });
+	const vectorIds = vectorQuery.matches.map((vec) => vec.vectorId);
+
+	// query D1 with the IDs to receive the actual content
+	if (!vectorIds.length) {
+		return c.json([]);
+	}
+
+	const relevantHighlightsContentQuery = `SELECT * FROM highlights where id IN (${vectorIds.join(', ')}) AND id <> (?) `;
+	const { results: relevantHighlightContents } = await c.env.DB.prepare(relevantHighlightsContentQuery).bind(highlightId).all();
+
+	// return the content to the calller
+	return c.json(relevantHighlightContents);
+});
+
 app.onError((err, c) => {
 	return c.text(err);
 });

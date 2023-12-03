0e3979205d756d30f57a8c74147133e4f80fb0ea
okezieuc <okezieuc@users.noreply.github.com>
2023-12-03 15:57:00 -0600

    display all highlights on app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [highlights, setHiglights] = useState([]);
+  const [relatedHighlights, setRelatedHighlights] = useState([]);
+
+  async function fetchAllHighlights() {
+    try {
+      const response = await fetch("/api/all");
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const jsonData = await response.json();
+      setHiglights(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllHighlights();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex-container">
+      <div class="flex-column">
+        <h2>My Highlights</h2>
+        {highlights.map((highlight) => (
+          <Highlight text={highlight.text} id={highlight.id} />
+        ))}
+      </div>
+
+      <div class="flex-column">
+        <h2>Related</h2>
+      </div>
     </div>
   );
+}
+
+function Highlight({ text, id }) {
+  return <div className="highlight">This: {text}</div>;
 }
 
 export default App;
